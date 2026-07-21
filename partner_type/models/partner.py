f7271751e5451a55203b6d93eac079d52b2f2734
ibrahimkaddour <admin@autochapeau.com>
2026-07-21 15:20:07 +0300

    change field name contact_type

--- partner_type/models/partner.py
+++ partner_type/models/partner.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class PartnerType(models.Model):
     _inherit = 'res.partner'
 
-    partner_type = fields.Selection(
+    contact_type = fields.Selection(
         [('customer', 'Customer'),
          ('supplier', 'Supplier'),
          ('employee', 'Employee')], default='customer')
