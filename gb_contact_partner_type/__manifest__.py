ea6bfa9bfcc3a6231ad79fd27b45f9d2b9d1b810
ibrahimkaddour <admin@autochapeau.com>
2026-07-21 16:09:24 +0300

    new module gb_contact_partner_type

---
+++ gb_contact_partner_type/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Contact Partner Type",
+    "version": "17.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["base",],
+    "author": "Gulfboost",
+    "category": "Base",
+    "data": [
+        'views/partner_inherit.xml'
+    ],
+}
---
+++ gb_contact_partner_type/models/partner_inherit.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api
+
+
+class PartnerInheritType(models.Model):
+    _inherit = 'res.partner'
+
+    contact_partner_type = fields.Selection(
+        [('customer', 'Customer'),
+         ('supplier', 'Supplier'),
+         ('employee', 'Employee')], default='customer')
