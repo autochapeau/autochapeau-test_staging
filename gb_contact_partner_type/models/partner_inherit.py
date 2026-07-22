604842cd654f8b6dacaa27560c76921cb9f9e987
ibrahimkaddour <admin@autochapeau.com>
2026-07-22 10:58:13 +0300

    updatee

--- gb_contact_partner_type/models/partner_inherit.py
+++ gb_contact_partner_type/models/partner_inherit.py
@@ -7,4 +7,4 @@ class PartnerInheritType(models.Model):
     contact_partner_type = fields.Selection(
         [('customer', 'Customer'),
          ('supplier', 'Supplier'),
-         ('employee', 'Employee')], default='customer')
+         ('employee', 'Employee')], default='customer', string='Contact **')
