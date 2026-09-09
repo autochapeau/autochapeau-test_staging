7141ef6f4d8021a914477d084fe03c45b1549a22
ibrahimkaddour <admin@autochapeau.com>
2026-09-09 15:23:17 +0300

    update

--- portal_api/controllers/contact_us.py
+++ portal_api/controllers/contact_us.py
@@ -78,11 +78,22 @@ class ContactUs(http.Controller):
         check_data = check_params(data, ["name", "email_from", "subject", "description"])
         if check_data:
             return make_json_response(422, check_data)
-        city = ''
-        if data.get("city_id"):
-            city_name = request.env['res.city'].sudo().search([('id', '=', data.get("city_id"))])
-            city += city_name
         try:
+            city = ""
+            country_id = data.get("country_id") or False
+            city_id = data.get("city_id")
+            if city_id:
+                city_rec = request.env["res.city"].sudo().browse(int(city_id))
+                if not city_rec.exists():
+                    return make_json_response(422, {"message": "city_id invalid"})
+                if country_id and city_rec.country_id.id != int(country_id):
+                    return make_json_response(422, {"message": "city does not belong to country"})
+                city = city_rec.name
+
+            tag_ids = data.get("tag_ids") or []
+            if tag_ids and not isinstance(tag_ids, list):
+                return make_json_response(422, {"message": "tag_ids must be a list"})
+
             values = {
                 "contact_name": data.get("name"),
                 "mobile": data.get("phone"),
@@ -90,10 +101,11 @@ class ContactUs(http.Controller):
                 "partner_name": data.get("partner_name"),
                 "name": data.get("subject"),
                 "description": data.get("description"),
-                "country_id": data.get("country_id"),
-                "city": city
+                "country_id": int(country_id) if country_id else False,
+                "city": city,
+                "tag_ids": [(6, 0, [int(tag_id) for tag_id in tag_ids])],
             }
             request.env["crm.lead"].with_user(SUPERUSER_ID).create(values)
             return make_json_response(200, "Lead created successfully")
         except Exception as e:
-            return make_json_response(500, {"message": e})
+            return make_json_response(500, {"message": str(e)})
